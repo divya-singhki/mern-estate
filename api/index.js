96b086ef2bf6b4b0438d52b85e86e66635b5b214
divya-singhki <divya.singh75309556@gmail.com>
2024-09-29 21:45:59 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,15 @@
-import express from 'express'
+import express from 'express';
+import mongoose from 'mongoose';
+
+mongoose.connect("mongodb://127.0.0.1:27017/estate").then(()=>{
+    console.log('connected to MongoDB!');
+})
+.catch((err)=>{
+    console.log(err);
+    
+})
+    
+
 const app= express();
 app.listen(3000,()=>{
     console.log('server is running on port 3000 !!!');
