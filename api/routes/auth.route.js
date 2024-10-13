46d10ada271639e69126f044e631b28343300053
divya-singhki <divya.singh75309556@gmail.com>
2024-10-13 17:55:11 +0530

    complete sign in page functionality

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -3,5 +3,5 @@ import { signin ,signup } from '../controllers/auth.controller.js';
 const router= express.Router();
 
 router.post("/signup",signup);
-router.post("/signin",signin);
+router.post("/signIn",signin);
 export default router;
