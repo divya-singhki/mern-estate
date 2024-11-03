8d15382b1e04eaabe920bc7f15c59e815fbab86d
divya-singhki <divya.singh75309556@gmail.com>
2024-11-03 15:54:43 +0530

    create  update user api route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -18,6 +18,7 @@ export default function App() {
       <Route path="/about" element={<About />}/>
       <Route element={<PrivateRoute/>}>
       <Route path="/profile" element={<Profile />}/>
+    
     </Route>
     </Routes>
     </BrowserRouter>
