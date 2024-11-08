dd1afd58e7adbf69de40b7307ff2a0fa1cd6f860
divya-singhki <divya.singh75309556@gmail.com>
2024-11-08 17:41:24 +0530

    add sign out functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,7 @@
 
 import { useSelector } from "react-redux"
 import {useRef, useState} from 'react'
-import{updateUserStart, updateUserSuccess,updateUserFaliure} from '../redux/user/userSlice';
+import{updateUserStart, updateUserSuccess,updateUserFaliure, deleteUserFailure, deleteUserStart, deleteUserSuccess, signOutUserStart} from '../redux/user/userSlice';
 import { useDispatch } from "react-redux";
 
 
@@ -41,6 +41,40 @@ setUpdateSuccess(true);
  }
  }
 
+ const handleDeleteUser =async()=>{
+  try {
+    dispatch(deleteUserStart());
+    const res= await fetch(`/api/user/delete/${currentUser._id}`,{
+      method:'DELETE',
+    });
+    const data =await res.json();
+    if(data.success===false){
+      dispatch(deleteUserFailure(data.message));
+      return;
+    }
+   dispatch(deleteUserSuccess(data));
+
+
+  } catch (error) {
+    dispatch(deleteUserFailure(error.message));
+  }
+ };
+
+const handleSignOut= async()=>{
+  try {
+    dispatch( signOutUserStart());
+    const res = await fetch('/api/auth/signout');
+    const data = await res.json();
+    if(data.success===false){
+      dispatch(deleteUserFailure(data.message));
+      return;
+    }
+    dispatch(deleteUserSuccess(data));
+
+  } catch (error) {
+    dispatch(deleteUserFailure(data.message));
+  }
+}
 
   return (
     <div className="p-3 max-w-lg mx-auto">
@@ -84,10 +118,10 @@ setUpdateSuccess(true);
       </form>
 
       <div className="flex justify-between mt-5">
-        <span className="text-red-700 cursor-pointer">
+        <span onClick={handleDeleteUser} className="text-red-700 cursor-pointer">
           Delete account
         </span>
-        <span className="text-red-700 cursor-pointer">
+        <span onClick={handleSignOut} className="text-red-700 cursor-pointer">
           Sign out
         </span>
       </div>
